f5ad41b56d53a4d154f98ab191f8c2118b562909
azorius <if20b251@technikum-wien.at>
2021-11-11 20:04:50 +0100

    clientside: send

--- Client/Client.cpp
+++ Client/Client.cpp
@@ -133,7 +133,7 @@ void deleteMessage(int create_socket) {
     char recvMessage[BUF];
 
     printf("Enter a username: ");
-    if (fgets(username, BUF, stdin) != NULL) {
+    if (fgets(username, 8, stdin) != NULL) {
         int size = strlen(username);
         // remove new-line signs from string at the end
         if (username[size - 2] == '\r' && username[size - 1] == '\n') {
@@ -147,7 +147,7 @@ void deleteMessage(int create_socket) {
         exit(EXIT_FAILURE);
     }
     printf("Enter the message number: ");
-    if (fgets(msgNumber, BUF, stdin) != NULL) {
+    if (fgets(msgNumber, 5, stdin) != NULL) {
         int size = strlen(msgNumber);
         // remove new-line signs from string at the end
         if (msgNumber[size - 2] == '\r' && msgNumber[size - 1] == '\n') {
@@ -185,7 +185,7 @@ void readMessage(int create_socket) {
     char recvMessage[BUF];
 
     printf("Enter a username: ");
-    if (fgets(username, BUF, stdin) != NULL) {
+    if (fgets(username, 8, stdin) != NULL) {
         int size = strlen(username);
         // remove new-line signs from string at the end
         if (username[size - 2] == '\r' && username[size - 1] == '\n') {
@@ -199,7 +199,7 @@ void readMessage(int create_socket) {
         exit(EXIT_FAILURE);
     }
     printf("Enter the message number: ");
-    if (fgets(msgNumber, BUF, stdin) != NULL) {
+    if (fgets(msgNumber, 5, stdin) != NULL) {
         int size = strlen(msgNumber);
         // remove new-line signs from string at the end
         if (msgNumber[size - 2] == '\r' && msgNumber[size - 1] == '\n') {
@@ -236,7 +236,7 @@ void listMessage(int create_socket) {
     char recvMessage[BUF];
 
     printf("Enter a username: ");
-    if (fgets(username, BUF, stdin) != NULL) {
+    if (fgets(username, 8, stdin) != NULL) {
         int size = strlen(username);
         // remove new-line signs from string at the end
         if (username[size - 2] == '\r' && username[size - 1] == '\n') {
@@ -265,5 +265,76 @@ void listMessage(int create_socket) {
 }
 
 void sendMessage(int create_socket) {
+    char message[BUF];
+    char sender[8];
+    char receiver[8];
+    char subject[80];
+    char recvMessage[BUF];
+    char *c = nullptr;
 
+    printf("Enter a username for the sender: ");
+    if(fgets(sender, 8, stdin) != NULL) {
+        int size = strlen(sender);
+        // remove new-line signs from string at the end
+        if (sender[size - 2] == '\r' && sender[size - 1] == '\n') {
+            size -= 2;
+            sender[size] = 0;
+        } else if (sender[size - 1] == '\n') {
+            --size;
+            sender[size] = 0;
+        }
+    }else{
+        exit(EXIT_FAILURE);
+    }
+    printf("Enter a username for the receiver: ");
+    if (fgets(receiver, 8, stdin) != NULL) {
+        int size = strlen(receiver);
+        // remove new-line signs from string at the end
+        if (receiver[size - 2] == '\r' && receiver[size - 1] == '\n') {
+            size -= 2;
+            receiver[size] = 0;
+        } else if (receiver[size - 1] == '\n') {
+            --size;
+            receiver[size] = 0;
+        }
+    }else{
+        exit(EXIT_FAILURE);
+    }
+    printf("Enter the subject of the message: ");
+    if (fgets(subject, 80, stdin) != NULL) {
+        int size = strlen(subject);
+        // remove new-line signs from string at the end
+        if (subject[size - 2] == '\r' && subject[size - 1] == '\n') {
+            size -= 2;
+            subject[size] = 0;
+        } else if (subject[size - 1] == '\n') {
+            --size;
+            subject[size] = 0;
+        }
+    }else{
+        exit(EXIT_FAILURE);
+    }
+    strcat(message, sender);
+    strcat(message, "\n");
+    strcat(message, receiver);
+    strcat(message, "\n");
+    strcat(message, subject);
+    strcat(message, "\n");
+
+    printf("Enter the message. Include a dot ('.') in a sentence to exit: ");
+    do{
+        *c = getchar();
+        strcat(message, c);
+    }while(*c != '.');
+    strcat(message, "\n");
+
+    if(send(create_socket, message, strlen(message), 0) == -1) {
+        perror("LIST failed to send!");
+        exit(EXIT_FAILURE);
+    }
+    if(recv(create_socket, recvMessage, strlen(recvMessage), 0) == -1){
+        perror("LIST failed to receive!");
+        exit(EXIT_FAILURE);
+    }
+    printf(recvMessage);
 }
